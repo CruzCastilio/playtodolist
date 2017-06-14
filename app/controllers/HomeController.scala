c99f8ebecbd8260453287169b3fa84e5865255d8
CruzCastilio <cruz.castillio@gmail.com>
2017-06-15 00:46:42 +0700

    Patch 3.4 Added error messages, redirect with FormWithErrors.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -20,10 +20,9 @@ class HomeController @Inject()(taskDao: TaskDao, val messagesApi: MessagesApi) e
     Ok(views.html.tasklist(taskDao.all()))
   }
 
-
   def taskPost = Action { implicit request =>
     taskForm.bindFromRequest.fold(
-      formWithErrors => BadRequest("WRONG!!!"),
+      formWithErrors => BadRequest(views.html.taskedit(None, formWithErrors)),
       task => {
         taskDao.create(task.label, task.task, new Date(), task.expirationDate, task.assigner, task.executor)
         Redirect(routes.HomeController.taskList()).flashing("success" -> "The task has been created!")
@@ -35,21 +34,21 @@ class HomeController @Inject()(taskDao: TaskDao, val messagesApi: MessagesApi) e
     taskDao.findById(id) match {
       case Some(found) =>
         taskForm.bindFromRequest.fold(
-          formWithErrors => BadRequest("WRONG!!!"),
+          formWithErrors => BadRequest(views.html.taskedit(Some((found.id, found.creationDate)), formWithErrors)),
           task => {
             taskDao.edit(id, task.label, task.task, task.expirationDate, task.assigner, task.executor)
             Redirect(routes.HomeController.taskList()).flashing("success" -> "The task has been edited!")
           }
         )
-      case None => BadRequest("Not Found")
+      case None => BadRequest("Not Found 404")
     }
   }
 
   def taskDetails(id: Long) = Action { implicit request =>
     taskDao.findById(id) match {
-      case obj @ Some(task) => Ok(views.html.taskedit(Some((task.id, task.creationDate)),taskForm.fill(TaskForm(
+      case obj @ Some(task) => Ok(views.html.taskedit(Some((task.id, task.creationDate)), taskForm.fill(TaskForm(
         task.label, task.task, task.expirationDate, task.assigner, task.executor))))
-      case None => BadRequest("Not Found")
+      case None => BadRequest("Not Found 404")
     }
   }
 
@@ -64,11 +63,12 @@ class HomeController @Inject()(taskDao: TaskDao, val messagesApi: MessagesApi) e
 
   val taskForm = Form(
     mapping(
-      "label" -> nonEmptyText,
-      "task" -> nonEmptyText,
-      "expirationDate" -> date,
-      "assigner" -> nonEmptyText,
-      "executor" -> nonEmptyText
+      "label" -> text.verifying("From 3 to 30 characters.", s => s.length >=3 && s.length <= 30),
+      "task" -> text.verifying("Shouldn't be empty.", _.trim.nonEmpty),
+      "expirationDate" -> date.verifying("Invalid expiration date.", _.after(new Date())),
+      "assigner" -> text.verifying("From 2 to 15 characters.", s => s.length >=2 && s.length <= 15),
+      "executor" -> text.verifying("From 2 to 15 characters.", s => s.length >=2 && s.length <= 15)
     )(TaskForm.apply)(TaskForm.unapply)
   )
+
 }
