ce93c0468614861d9156c2277a20fd0a13b0b77e
CruzCastilio <cruz.castillio@gmail.com>
2017-06-23 18:35:49 +0700

    Patch 4.2 Added logging with log4j2.

--- app/ErrorHandler.scala
+++ app/ErrorHandler.scala
@@ -1,11 +1,12 @@
 import javax.inject._
 
-import play.api.http.DefaultHttpErrorHandler
+import org.apache.logging.log4j.scala.Logging
 import play.api._
+import play.api.http.DefaultHttpErrorHandler
 import play.api.http.Status.{BAD_REQUEST, FORBIDDEN, NOT_FOUND}
 import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.mvc._
 import play.api.mvc.Results._
+import play.api.mvc._
 import play.api.routing.Router
 
 import scala.concurrent._
@@ -16,22 +17,33 @@ class ErrorHandler @Inject() (val messagesApi: MessagesApi)(
   config: Configuration,
   sourceMapper: OptionalSourceMapper,
   router: Provider[Router]
-) extends DefaultHttpErrorHandler(env, config, sourceMapper, router) with I18nSupport {
+) extends DefaultHttpErrorHandler(env, config, sourceMapper, router) with I18nSupport with Logging {
 
-  override def onClientError(request: RequestHeader, statusCode: Int, message: String) =
+  override def onClientError(request: RequestHeader, statusCode: Int, message: String) = {
+    logger.info("Client Error Has Occurred")
     statusCode match {
-      case BAD_REQUEST => Future.successful(BadRequest(views.html.errors(messagesApi("badRequestUpperCase"))))
-      case FORBIDDEN => Future.successful(Forbidden(views.html.errors(messagesApi("forbidden"))))
-      case NOT_FOUND => Future.successful(NotFound(views.html.errors(messagesApi("notFoundUpperCase"))))
+      case BAD_REQUEST =>
+        logger.info("Client Error Bad Request Has Occurred")
+        Future.successful(BadRequest(views.html.errors(messagesApi("badRequestUpperCase"))))
+      case FORBIDDEN =>
+        logger.info("Client Error Forbidden Has Occurred")
+        Future.successful(Forbidden(views.html.errors(messagesApi("forbidden"))))
+      case NOT_FOUND =>
+        logger.info("Client Error Not Found Has Occurred")
+        Future.successful(NotFound(views.html.errors(messagesApi("notFoundUpperCase"))))
       case clientError if statusCode >= 400 && statusCode < 500 =>
+        logger.info("Client Error Some Error Has Occurred")
         Future.successful(Results.Status(clientError)(views.html.defaultpages.badRequest(request.method, request.uri, message)))
       case nonClientError =>
+        logger.info("Non Client Error Has Occurred")
         throw new IllegalArgumentException("onClientError " + messagesApi("nonClientError") + s" $statusCode: $message")
     }
+  }
 
-
-  override def onServerError(request: RequestHeader, exception: Throwable) =
+  override def onServerError(request: RequestHeader, exception: Throwable) = {
+    logger.info("Server Error Has Occurred")
     Future.successful(
       InternalServerError(messagesApi("onServerError") + exception.getMessage)
     )
+  }
 }
--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,27 +4,34 @@ import java.util.Date
 import javax.inject._
 
 import models._
+import org.apache.logging.log4j.scala.Logging
 import play.api.data.Forms._
 import play.api.data._
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
 
-
 @Singleton
-class HomeController @Inject()(taskDao: TaskDao, val messagesApi: MessagesApi) extends Controller with I18nSupport {
+class HomeController @Inject()(taskDao: TaskDao, val messagesApi: MessagesApi) extends Controller with I18nSupport with Logging {
 
-  def index = Action {
+  def index = Action { implicit request =>
+    logger.info("Home Page Attempt")
     Redirect(routes.HomeController.taskList())
   }
 
   def taskList = Action { implicit request =>
+    logger.info("Task List Opening")
     Ok(views.html.tasklist(taskDao.all()))
   }
 
   def taskPost = Action { implicit request =>
+    logger.info("New Task Creating Attempt")
     taskForm.bindFromRequest.fold(
-      formWithErrors => BadRequest(views.html.taskedit(None, formWithErrors)),
+      formWithErrors => {
+        logger.info("Got Form Errors While New Task Creation")
+        BadRequest(views.html.taskedit(None, formWithErrors))
+      },
       task => {
+        logger.info("Successful New Task Creation")
         taskDao.create(task.label, task.task, new Date(), task.expirationDate, task.assigner, task.executor)
         Redirect(routes.HomeController.taskList()).flashing("success" -> messagesApi("flashCreated"))
       }
@@ -32,32 +39,47 @@ class HomeController @Inject()(taskDao: TaskDao, val messagesApi: MessagesApi) e
   }
 
   def taskEdit(id: Long) = Action { implicit request =>
+    logger.info("Task ID Searching Attempt")
     taskDao.findById(id) match {
       case Some(found) =>
+        logger.info("Task ID Found; Task Editing Attempt")
         taskForm.bindFromRequest.fold(
-          formWithErrors => BadRequest(views.html.taskedit(Some((found.id, found.creationDate)), formWithErrors)),
+          formWithErrors => {
+            logger.info("Got Form Errors While Task Editing")
+            BadRequest(views.html.taskedit(Some((found.id, found.creationDate)), formWithErrors))
+          },
           task => {
+            logger.info("Successful Task Editing")
             taskDao.edit(id, task.label, task.task, task.expirationDate, task.assigner, task.executor)
             Redirect(routes.HomeController.taskList()).flashing("success" -> messagesApi("flashEdited"))
           }
         )
-      case None => BadRequest
+      case None =>
+        logger.info("Task ID Not Found")
+        BadRequest(views.html.errors(messagesApi("wrongId")))
     }
   }
 
   def taskDetails(id: Long) = Action { implicit request =>
+    logger.info("Task ID Searching Attempt")
     taskDao.findById(id) match {
-      case Some(task) => Ok(views.html.taskedit(Some((task.id, task.creationDate)), taskForm.fill(TaskForm(
-        task.label, task.task, task.expirationDate, task.assigner, task.executor))))
-      case None => BadRequest(views.html.errors(messagesApi("wrongId")))
+      case Some(task) =>
+        logger.info("Task Details Opened")
+        Ok(views.html.taskedit(Some((task.id, task.creationDate)), taskForm.fill(TaskForm(
+          task.label, task.task, task.expirationDate, task.assigner, task.executor))))
+      case None =>
+        logger.info("Task ID Not Found")
+        BadRequest(views.html.errors(messagesApi("wrongId")))
     }
   }
 
   def taskNew = Action { implicit request =>
+    logger.info("New Task Creation Page Opened")
     Ok(views.html.taskedit(None, taskForm))
   }
 
   def taskDelete(id: Long) = Action { implicit request =>
+    logger.info("Successful Task Deleting")
     taskDao.delete(id)
     Redirect(routes.HomeController.taskList()).flashing("success" -> messagesApi("flashDeleted"))
   }
